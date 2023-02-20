a7db5c7a43c76db7ee98c0954436a241a89039fa
jz423 <jz423@duke.edu>
2023-02-20 01:00:17 -0500

    need some functions

--- src/HttpMessage.h
+++ src/HttpMessage.h
@@ -21,14 +21,19 @@ public:
 
   std::string getVersion() const;
 
+  //For both request and response
   std::string getCacheControl() const;
 
+  //For both request and response
   std::string getMaxAge() const;
 
+  //Only in response
   std::string getExpires() const;
 
+  //Only in response
   std::string getLastModified() const;
 
+  //For response
   std::string getDate() const;
 };
 
